4a793254efd04fda00f95201d233135518e91379
John Nieri <niericentral@gmail.com>
2013-11-27 19:30:56 +0900

    Left kwargs at the top level of mazemakers but added explanations in the docstring.

--- polymaze/mazemakers.py
+++ polymaze/mazemakers.py
@@ -5,7 +5,12 @@ import maze as _maze
 def rectangle(**kwargs):
     """Generate a rectangular maze.
 
-    kwargs are defined in the gridmakers module.
+    kwargs:
+    supershape - a supershape class that defines how the maze will look
+    complexity - scale the difficulty of the maze to any positive number
+                 e.g. 0.5 is very simple, 1000 is very difficult
+    aspect_h - relative height of the result image (square if only one provided)
+    aspect_w - relative width of the result image (square if only one provided)
     """
     rectangle_grid = _gridmakers.rectangle(**kwargs)
     return _maze.Maze(rectangle_grid)
@@ -14,7 +19,12 @@ def rectangle(**kwargs):
 def string(s, **kwargs):
     """Generate a the character and maze for each character in s.
 
-    kwargs are defined in the gridmakers module.
+    kwargs:
+    supershape - a supershape class that defines how the maze will look
+    complexity - scale the difficulty of the maze to any positive number
+                 e.g. 0.5 is very simple, 1000 is very difficult
+    aspect_h - relative height of the result image (square if only one provided)
+    aspect_w - relative width of the result image (square if only one provided)
     """
     # get each non white-space character and convert to a maze
     for word in s.split():
