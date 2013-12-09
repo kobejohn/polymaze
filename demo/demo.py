3bb2dc23353c3c29e0c3555e6aa3c1cec378c6de
John Nieri <niericentral@gmail.com>
2013-12-10 00:10:20 +0900

    Improved demo to demonstrate levels of complexity explicitly.

--- demo/demo.py
+++ demo/demo.py
@@ -5,29 +5,33 @@ import polymaze as pmz
 
 def demo():
     print 'Saving demos:'
-    rectangle_maze()
-    string_maze()
+    basic_string_maze()
+    custom_rectangle_mazes()
 
 
-def string_maze():
+def basic_string_maze():
     s = u'Poly Maze'
     grid = pmz.PolyGrid()
-    grid.create_string(s)
+    grid.create_string(s, complexity=2)
     maze = pmz.Maze(grid)
     filename = 'Demo Normal String.png'
     maze.image().save(filename, format='PNG')
     print filename
 
 
-def rectangle_maze():
-    easy = 1.0
-    close_to_golden_rectangle = 0.625
-    grid = pmz.PolyGrid()
-    grid.create_rectangle(complexity=easy, aspect=close_to_golden_rectangle)
-    maze = pmz.Maze(grid)
-    filename = 'Demo Easy Rectangle.png'
-    maze.image().save(filename, format='PNG')
-    print filename
+def custom_rectangle_mazes():
+    # prepare some custom items
+    aspect_close_to_golden_rectangle = 0.625
+    for i, (ss_name, ss) in enumerate(pmz.SUPERSHAPES_DICT.items()):
+        complexity = i + 1
+        grid = pmz.PolyGrid(supershape=ss)
+        grid.create_rectangle(complexity=complexity,
+                              aspect=aspect_close_to_golden_rectangle)
+        maze = pmz.Maze(grid)
+        filename = 'Demo (complexity {}) ({}).png'.format(complexity,
+                                                          ss_name)
+        maze.image().save(filename, format='PNG')
+        print filename
 
 
 if __name__ == '__main__':
